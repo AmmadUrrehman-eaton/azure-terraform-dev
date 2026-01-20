373df0354c1360ecc989d1075a1c32923f6249ef
Ammad Ur Rehman <ammadurrehman@eaton.com>
2026-01-20 08:57:36 -0500

    str acc commit

--- modules/main.tf
+++ modules/main.tf
@@ -2,3 +2,16 @@ resource "azurerm_resource_group" "rg_demo" {
   name     = "rg-terraform-demo-dev"
   location = "eastus"
 }
+
+resource "azurerm_storage_account" "storage_demo" {
+  name                     = "stterraformdemodev001"
+  resource_group_name      = azurerm_resource_group.rg_demo.name
+  location                 = azurerm_resource_group.rg_demo.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+    managed_by  = "terraform"
+  }
+}
