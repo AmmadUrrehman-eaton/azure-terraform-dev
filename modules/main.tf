82c9a66a2f79d9ccb239d0f2d97e945a4d8540e5
Ammad Ur Rehman <ammadurrehman@eaton.com>
2026-01-20 09:49:33 -0500

    Remove provider binaries; add Terraform .gitignore

--- modules/main.tf
+++ modules/main.tf
@@ -3,15 +3,26 @@ resource "azurerm_resource_group" "rg_demo" {
   location = "eastus"
 }
 
-resource "azurerm_storage_account" "storage_demo" {
-  name                     = "stterraformdemodev001"
+
+# Reuse the existing RG you already created
+# resource "azurerm_resource_group" "rg_demo" {
+#   name     = "rg-terraform-demo-dev"
+#   location = "eastus"
+# }
+
+# 1) Storage Account (unique name!)
+resource "azurerm_storage_account_test_123321" "st_demo" {
+  name                     = "st${replace(azurerm_resource_group.rg_demo.name, "-", "")}01" # adjust if needed to meet naming rules
   resource_group_name      = azurerm_resource_group.rg_demo.name
   location                 = azurerm_resource_group.rg_demo.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+  # Optional good practices
+  allow_blob_public_access = false
+  min_tls_version          = "TLS1_2"
   tags = {
-    environment = "dev"
-    managed_by  = "terraform"
+    env = "dev"
+    owner = "terraform"
   }
 }
