009d6000fe1221225e769de7d53679a5ae2a0dd1
Ammad Ur Rehman <ammadurrehman@eaton.com>
2026-01-20 10:28:53 -0500

    Use existing RG via data source; create storage account only

--- modules/main.tf
+++ modules/main.tf
@@ -1,24 +1,20 @@
 
+# Use the existing RG instead of creating it
+data "azurerm_resource_group" "rg_demo" {
+  name = "rg-terraform-demo-dev"
+}
 
-# Reuse the existing RG you already created
-# resource "azurerm_resource_group" "rg_demo" {
-#   name     = "rg-terraform-demo-dev"
-#   location = "eastus"
-# }
-
-
-
+# Storage Account (must be globally unique, all lowercase, 3–24 chars)
 resource "azurerm_storage_account" "st_demo" {
-  name                     = "stammaddemo1234"  # must be globally unique, lowercase, 3–24 chars
-  resource_group_name      = azurerm_resource_group.rg_demo.name
-  location                 = azurerm_resource_group.rg_demo.location
+  name                     = "stammaddemo1234" # change to a unique name if needed
+  resource_group_name      = data.azurerm_resource_group.rg_demo.name
+  location                 = data.azurerm_resource_group.rg_demo.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # Removed: allow_blob_public_access (not supported in your provider)
-  # Optional hardening that is supported:
-  min_tls_version              = "TLS1_2"
-  public_network_access_enabled = true
+  # Removed unsupported "allow_blob_public_access"
+  min_tls_version                = "TLS1_2"
+  public_network_access_enabled  = true
 
   tags = {
     env   = "dev"
