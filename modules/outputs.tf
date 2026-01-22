c3792ab21fb56c93f03eeaf344602441869ba4a4
Ammad Ur Rehman <ammadurrehman@eaton.com>
2026-01-22 11:44:41 -0500

    Add VNet, subnets, and Private DNS zone/link for App Service private endpoints

---
+++ modules/outputs.tf
@@ -0,0 +1,25 @@
+
+output "vnet_id" {
+  description = "ID of the VNet"
+  value       = azurerm_virtual_network.vnet.id
+}
+
+output "vnet_name" {
+  description = "Name of the VNet"
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "subnet_app_id" {
+  description = "ID of the app/workload subnet"
+  value       = azurerm_subnet.snet_app.id
+}
+
+output "subnet_pe_id" {
+  description = "ID of the private endpoint subnet"
+  value       = azurerm_subnet.snet_pe.id
+}
+
+output "private_dns_zone_appservice" {
+  description = "Private DNS zone name used for App Service/Function private endpoints"
+  value       = azurerm_private_dns_zone.appservice.name
+}
---
+++ modules/variables.tf
@@ -0,0 +1,66 @@
+
+variable "location" {
+  type        = string
+  description = "Azure region for all resources"
+  default     = "eastus"
+}
+
+variable "rg_name" {
+  type        = string
+  description = "Existing resource group name where networking will be deployed"
+  default     = "rg-terraform-demo-dev"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "Virtual network name"
+  default     = "vnet-terraform-demo-dev"
+}
+
+variable "vnet_address_space" {
+  type        = list(string)
+  description = "Address space for VNet"
+  default     = ["10.50.0.0/16"]
+}
+
+variable "subnet_app_name" {
+  type        = string
+  description = "Subnet for future app service integration or workloads"
+  default     = "snet-app"
+}
+
+variable "subnet_app_prefix" {
+  type        = list(string)
+  description = "CIDR for the app subnet"
+  default     = ["10.50.1.0/24"]
+}
+
+variable "subnet_pe_name" {
+  type        = string
+  description = "Subnet dedicated to Private Endpoints"
+  default     = "snet-private-endpoints"
+}
+
+variable "subnet_pe_prefix" {
+  type        = list(string)
+  description = "CIDR for the private endpoint subnet"
+  default     = ["10.50.2.0/24"]
+}
+
+# Private DNS zone names for App Service/Function private endpoints
+# Web Apps / Function Apps use azurewebsites.net as the base; PE uses privatelink.azurewebsites.net
+variable "appservice_privatedns_zone" {
+  type        = string
+  description = "Private DNS zone for App Service/Function private endpoints"
+  default     = "privatelink.azurewebsites.net"
+}
+
+# Optional tags
+variable "tags" {
+  type        = map(string)
+  description = "Common tags"
+  default = {
+    env   = "dev"
+    owner = "terraform"
+  }
+}
